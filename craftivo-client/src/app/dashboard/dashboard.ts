67360fd73529abc25a5cc9fbc4a73872fe86ef66
abosmraa <samerfakih15@gmail.com>
2025-09-03 17:18:10 +0300

    Add dashboard layout and styles with router outlet integration

--- craftivo-client/src/app/dashboard/dashboard.ts
+++ craftivo-client/src/app/dashboard/dashboard.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
 import { Sidebar } from '../sidebar/sidebar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Navbar, Sidebar],
+  imports: [Navbar, Sidebar, RouterModule],
   templateUrl: './dashboard.html',
   styleUrls: ['./dashboard.css'],
 })
