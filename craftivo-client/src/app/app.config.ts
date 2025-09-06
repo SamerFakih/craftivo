4e97e944716f64ed038df52bd6afa5bce1bb3fa1
abosmraa <samerfakih15@gmail.com>
2025-09-06 05:30:47 +0300

    Refactor PrimeNG theme configuration to use definePreset for improved semantic styling

--- craftivo-client/src/app/app.config.ts
+++ craftivo-client/src/app/app.config.ts
@@ -7,6 +7,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { providePrimeNG } from 'primeng/config';
 import { provideRouter } from '@angular/router';
 import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -20,7 +21,48 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: definePreset(Aura, {
+          semantic: {
+            primary: {
+              50: '{teal.50}',
+              100: '{teal.100}',
+              200: '{teal.200}',
+              300: '{teal.300}',
+              400: '{teal.400}',
+              500: '{teal.500}',
+              600: '{teal.600}',
+              700: '{teal.700}',
+              800: '{teal.800}',
+              900: '{teal.900}',
+              950: '{teal.950}',
+            },
+          },
+          components: {
+            button: {
+              text: {
+                primary: {
+                  color: 'white',
+                },
+                secondary: {
+                  color: 'white',
+                  activeBackground: 'white',
+                },
+              },
+            },
+            progressbar: {
+              root: {
+                background: '{teal.300}',
+                borderRadius: '10px',
+              },
+              value: {
+                background: '{teal.700}',
+              },
+              label: {
+                color: 'white',
+              },
+            },
+          },
+        }),
       },
     }),
   ],
