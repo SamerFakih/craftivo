94641b833949980aa2c1851e29ae76f7cb8ba6b7
abosmraa <samerfakih15@gmail.com>
2025-09-10 10:53:03 +0300

    refactor: remove unnecessary ESLint disable comments for cleaner code

--- craftivo server/src/clients/clients.service.ts
+++ craftivo server/src/clients/clients.service.ts
@@ -1,8 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/require-await */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateClientDto } from './dto/create-clients.dto';
