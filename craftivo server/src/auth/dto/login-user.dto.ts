570918959077161d955a3365e362467b941cfa17
abosmraa <samerfakih15@gmail.com>
2025-09-10 06:48:08 +0300

    fix: update LoginUserDto for improved email handling and remove unnecessary validations

--- craftivo server/src/auth/dto/login-user.dto.ts
+++ craftivo server/src/auth/dto/login-user.dto.ts
@@ -1,14 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString, IsNotEmpty, MinLength } from 'class-validator';
+import { IsEmail, IsString, MinLength } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class LoginUserDto {
-  @ApiProperty({ description: 'User email', example: 'john.doe@example.com' })
+  @ApiProperty({
+    example: 'john@example.com',
+    description: 'User email address',
+  })
   @IsEmail()
+  @Transform(({ value }) => value.toLowerCase().trim())
   email: string;
 
-  @ApiProperty({ description: 'User password', example: 'strongpassword' })
+  @ApiProperty({
+    example: 'password123',
+    description: 'User password',
+    minLength: 6,
+  })
   @IsString()
-  @IsNotEmpty()
   @MinLength(6)
   password: string;
 }
