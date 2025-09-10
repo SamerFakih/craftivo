a9a23c76190d498cab18dcc491fecbb8bf89c116
abosmraa <samerfakih15@gmail.com>
2025-09-10 10:53:37 +0300

    refactor: enhance comments in ValidationPipe for improved clarity on data transformation and validation

--- craftivo server/src/common/pipes/validation.pipe.ts
+++ craftivo server/src/common/pipes/validation.pipe.ts
@@ -13,12 +13,15 @@ import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class ValidationPipe implements PipeTransform<any> {
+  // This method transforms and validates incoming data
   async transform(value: any, { metatype }: ArgumentMetadata) {
+    // If no metatype is provided or it's a primitive type, return the value as is
     if (!metatype || !this.toValidate(metatype)) {
       return value;
     }
     const object = plainToInstance(metatype, value);
     const errors = await validate(object);
+    // If validation errors exist, throw a BadRequestException with details
     if (errors.length > 0) {
       const messages = errors.map((error) => {
         return Object.values(error.constraints || {}).join(', ');
@@ -29,7 +32,7 @@ export class ValidationPipe implements PipeTransform<any> {
     }
     return value;
   }
-
+  // This method checks if the metatype is a class that requires validation
   private toValidate(metatype: Function): boolean {
     const types: Function[] = [String, Boolean, Number, Array, Object];
     return !types.includes(metatype);
