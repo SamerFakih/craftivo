6bf76613e9b59e2c22730a328307ea70b3abfd0f
abosmraa <samerfakih15@gmail.com>
2025-09-12 01:54:57 +0300

    Fetch team members from API and initialize members state

--- craftivo-client/src/app/team/team.ts
+++ craftivo-client/src/app/team/team.ts
@@ -1,7 +1,8 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component, signal, computed, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Member } from '../models/team';
 import { MemberCard } from '../components/member-card/member-card';
+import { TeamService } from '../services/team.service';
 
 @Component({
   selector: 'app-team-page',
@@ -10,62 +11,73 @@ import { MemberCard } from '../components/member-card/member-card';
   templateUrl: './team.html',
   styleUrls: ['./team.css'],
 })
-export class Team {
-  // demo data – replace with API
-  members = signal<Member[]>([
-    {
-      id: 'm1',
-      name: 'Sarah Johnson',
-      title: 'Senior Developer',
-      status: 'active',
-      email: 'sarah@freelancehub.com',
-      location: 'San Francisco, CA',
-      hourlyRateUSD: 80,
-      hoursMonth: 145,
-      activeProjects: 3,
-      tasksDone: 127,
-      skills: ['React', 'Node.js', 'Python', 'AWS'],
-    },
-    {
-      id: 'm2',
-      name: 'Mike Chen',
-      title: 'Frontend Engineer',
-      status: 'active',
-      email: 'mike@freelancehub.com',
-      location: 'Austin, TX',
-      hourlyRateUSD: 65,
-      hoursMonth: 132,
-      activeProjects: 2,
-      tasksDone: 98,
-      skills: ['Angular', 'TypeScript', 'Tailwind'],
-    },
-    {
-      id: 'm3',
-      name: 'Emma Davis',
-      title: 'Product Designer',
-      status: 'inactive',
-      email: 'emma@freelancehub.com',
-      location: 'Remote',
-      hourlyRateUSD: 70,
-      hoursMonth: 110,
-      activeProjects: 1,
-      tasksDone: 76,
-      skills: ['Figma', 'UX', 'Design Systems'],
-    },
-    {
-      id: 'm4',
-      name: 'John Ali',
-      title: 'Backend Engineer',
-      status: 'active',
-      email: 'john@freelancehub.com',
-      location: 'San Jose, CA',
-      hourlyRateUSD: 90,
-      hoursMonth: 168,
-      activeProjects: 3,
-      tasksDone: 141,
-      skills: ['Go', 'PostgreSQL', 'AWS'],
-    },
-  ]);
+export class Team implements OnInit {
+  constructor(private teamService: TeamService) {}
+  members = signal<Member[]>([]);
+  ngOnInit() {
+    this.teamService.getTeamMembers().subscribe({
+      next: (data) => {
+        console.log('Fetched members raw data:', data);
+        this.members.set(data);
+      },
+    });
+  }
+
+  // // demo data – replace with API
+  // members = signal<Member[]>([
+  //   {
+  //     id: 'm1',
+  //     name: 'Sarah Johnson',
+  //     title: 'Senior Developer',
+  //     status: 'active',
+  //     email: 'sarah@freelancehub.com',
+  //     location: 'San Francisco, CA',
+  //     hourlyRateUSD: 80,
+  //     hoursMonth: 145,
+  //     activeProjects: 3,
+  //     tasksDone: 127,
+  //     skills: ['React', 'Node.js', 'Python', 'AWS'],
+  //   },
+  //   {
+  //     id: 'm2',
+  //     name: 'Mike Chen',
+  //     title: 'Frontend Engineer',
+  //     status: 'active',
+  //     email: 'mike@freelancehub.com',
+  //     location: 'Austin, TX',
+  //     hourlyRateUSD: 65,
+  //     hoursMonth: 132,
+  //     activeProjects: 2,
+  //     tasksDone: 98,
+  //     skills: ['Angular', 'TypeScript', 'Tailwind'],
+  //   },
+  //   {
+  //     id: 'm3',
+  //     name: 'Emma Davis',
+  //     title: 'Product Designer',
+  //     status: 'inactive',
+  //     email: 'emma@freelancehub.com',
+  //     location: 'Remote',
+  //     hourlyRateUSD: 70,
+  //     hoursMonth: 110,
+  //     activeProjects: 1,
+  //     tasksDone: 76,
+  //     skills: ['Figma', 'UX', 'Design Systems'],
+  //   },
+  //   {
+  //     id: 'm4',
+  //     name: 'John Ali',
+  //     title: 'Backend Engineer',
+  //     status: 'active',
+  //     email: 'john@freelancehub.com',
+  //     location: 'San Jose, CA',
+  //     hourlyRateUSD: 90,
+  //     hoursMonth: 168,
+  //     activeProjects: 3,
+  //     tasksDone: 141,
+  //     skills: ['Go', 'PostgreSQL', 'AWS'],
+  //   },
+  // ]);
 
   // search
   q = signal('');
