4aaf4ddee4c301f6631aad88c824a984a2b861ab
abosmraa <samerfakih15@gmail.com>
2025-09-12 01:52:05 +0300

    Implement authentication check on app initialization

--- craftivo-client/src/app/app.ts
+++ craftivo-client/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,10 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   template: '<router-outlet></router-outlet>',
   styleUrl: './app.css',
 })
-export class App {}
+export class App implements OnInit {
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.checkAuthStatus();
+  }
+}
