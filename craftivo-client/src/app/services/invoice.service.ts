391ea365bb832525022f5f1ace3fc56e9283c74c
abosmraa <samerfakih15@gmail.com>
2025-09-12 01:53:52 +0300

    Add InvoiceService to handle invoice retrieval from API

---
+++ craftivo-client/src/app/services/invoice.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InvoiceService {
+  private apiUrl = 'http://localhost:3000/api/v1';
+
+  constructor(private http: HttpClient) {}
+
+  getInvoices(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/invoices`, { withCredentials: true });
+  }
+}
