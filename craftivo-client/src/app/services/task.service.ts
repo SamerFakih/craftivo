0060044ee10df99ffd14206572a49904cd2c0315
abosmraa <samerfakih15@gmail.com>
2025-09-12 03:29:36 +0300

    Refactor TaskService to use DataCacheService for task retrieval and cache invalidation

--- craftivo-client/src/app/services/task.service.ts
+++ craftivo-client/src/app/services/task.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { DataCacheService } from './data-cache.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:3000/api/v1';
-
-  constructor(private http: HttpClient) {}
+  constructor(private dataCacheService: DataCacheService) {}
 
   getTasks(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/tasks`, { withCredentials: true });
+    // Use cached data instead of direct HTTP calls
+    return this.dataCacheService.getTasks();
+  }
+
+  // Add method to invalidate cache when tasks are updated
+  invalidateCache(): void {
+    this.dataCacheService.invalidateTasks();
   }
 }
