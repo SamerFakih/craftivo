815dfdcdbc372f09f62c6b2c12475b4271229113
abosmraa <samerfakih15@gmail.com>
2025-09-12 03:29:49 +0300

    Refactor TimeTrackingService to utilize DataCacheService for time entry retrieval

--- craftivo-client/src/app/services/time-tracking.service.ts
+++ craftivo-client/src/app/services/time-tracking.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { DataCacheService } from './data-cache.service';
 
 @Injectable({
   providedIn: 'root',
@@ -8,9 +9,9 @@ import { Observable } from 'rxjs';
 export class TimeTrackingService {
   private apiUrl = 'http://localhost:3000/api/v1';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cacheService: DataCacheService) {}
 
   getTimeEntries(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/time-entries`, { withCredentials: true });
+    return this.cacheService.getTimeEntries();
   }
 }
