b07bce57f4650feafe7f78fd34f915888a946ea3
abosmraa <samerfakih15@gmail.com>
2025-09-12 04:28:27 +0300

    refactor: streamline AuthResponseDto by removing message and user object, enhancing clarity of user properties

--- craftivo server/src/auth/dto/auth-response.dto.ts
+++ craftivo server/src/auth/dto/auth-response.dto.ts
@@ -1,27 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { UserRole } from '@prisma/client';
 
 export class AuthResponseDto {
-  @ApiProperty({ example: 'User logged in successfully' })
-  message: string;
+  @ApiProperty({ description: 'User ID', example: 1 })
+  user_id: number;
 
-  @ApiProperty({
-    description: 'User information',
-    example: {
-      id: 1,
-      email: 'john@example.com',
-      first_name: 'John',
-      last_name: 'Doe',
-      role: 'freelancer',
-    },
-  })
-  user: {
-    id: number;
-    email: string;
-    first_name: string;
-    last_name: string;
-    role: UserRole;
-  };
+  @ApiProperty({ description: 'User email', example: 'user@example.com' })
+  email: string;
+
+  @ApiProperty({ description: 'User first name', example: 'John' })
+  firstName: string;
+
+  @ApiProperty({ description: 'User last name', example: 'Doe' })
+  lastName: string;
+
+  @ApiProperty({ description: 'User role', example: 'user' })
+  role: string;
 
   @ApiProperty({
     example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
