3813bf78a8a7d6a83d78ae928a1b2c44a7d9f4db
abosmraa <samerfakih15@gmail.com>
2025-09-21 08:43:41 +0300

    chore(ci): fix formatting in karma configuration for consistency

--- craftivo-client/karma.conf.js
+++ craftivo-client/karma.conf.js
@@ -8,16 +8,16 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: { clearContext: false },
     reporters: ['progress', 'coverage'],
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage'),
-      reporters: [ { type: 'html' }, { type: 'text-summary' }, { type: 'lcov' } ],
-      check: { global: { statements: 60, branches: 50, functions: 55, lines: 60 } }
+      reporters: [{ type: 'html' }, { type: 'text-summary' }, { type: 'lcov' }],
+      check: { global: { statements: 60, branches: 50, functions: 55, lines: 60 } },
     },
     browsers: ['ChromeHeadless'],
-    singleRun: true
+    singleRun: true,
   });
 };
