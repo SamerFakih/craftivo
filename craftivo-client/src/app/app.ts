84ac5b8bec61385edcece07a053445abc2d2fb22
abosmraa <samerfakih15@gmail.com>
2025-09-21 20:28:40 +0300

    fix(app): remove text from visually hidden header in app component template

--- craftivo-client/src/app/app.ts
+++ craftivo-client/src/app/app.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: '<h1 class="visually-hidden">App</h1><router-outlet></router-outlet>',
+  template: '<h1 class="visually-hidden"></h1><router-outlet></router-outlet>',
   styleUrls: ['./app.css'],
 })
 export class App {}
